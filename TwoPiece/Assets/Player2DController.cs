259771ee37ec5d8823f1fe3c405db5749b7d2e56
Samuel Braley <samuel.braley@utexas.edu>
2016-09-14 17:47:37 -0500

    added basic melee and gun attack and health

--- TwoPiece/Assets/Player2DController.cs
+++ TwoPiece/Assets/Player2DController.cs
@@ -10,11 +10,19 @@ namespace UnityStandardAssets._2D
     {
         private Animator m_Anim;            // Reference to the player's animator component.
         private Player2D m_Character;
+        int lastDir;
+        float playerWidth;
+        const float fireCooldown = 1.0f;
+        float currentFireCooldown = 0.0f;
 
         private void Awake()
         {
             m_Character = GetComponent<Player2D>();
             m_Anim = GetComponent<Animator>();
+            lastDir = 0;
+            BoxCollider2D hitbox = GetComponent<BoxCollider2D>();
+            //Rigidbody2D rigid = GetComponent<>
+            playerWidth = transform.localScale.x*(hitbox.size.x / 2);
         }
 
 
@@ -26,6 +34,7 @@ namespace UnityStandardAssets._2D
 
         private void FixedUpdate()
         {
+            currentFireCooldown -= Time.fixedDeltaTime;
             // Read the inputs.
             int dir = 0;
             if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
@@ -35,9 +44,17 @@ namespace UnityStandardAssets._2D
             bool jump = Input.GetKey(KeyCode.Space);
             bool crouch = Input.GetKey(KeyCode.LeftControl);
             bool dash = Input.GetKey(KeyCode.LeftShift);
+            bool fire = Input.GetKey(KeyCode.E);
+            bool melee = Input.GetKey(KeyCode.F);
 
+            if (dir != 0)
+            {
+                lastDir = dir;
+            }
             // Pass all parameters to the character control script.
             m_Character.Move(dir, crouch, jump, dash);
+            Fire(fire);
+            Melee(melee);
         }
 
         void OnTriggerEnter2D(Collider2D other)
@@ -56,5 +73,54 @@ namespace UnityStandardAssets._2D
                 m_Anim.SetBool("isClimbing", false);
             }
         }
+
+        void Fire(bool fire)
+        {
+            if (fire && currentFireCooldown <= 0.0f)
+            {
+                currentFireCooldown = fireCooldown;
+                RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x + (playerWidth*lastDir), transform.position.y), new Vector2(lastDir,0));
+                //Vector3[] array = { new Vector3(transform.position.x + (playerWidth * lastDir), transform.position.y,0),new Vector3(transform.position.x + (playerWidth * lastDir) + 20, transform.position.y, 0) };
+                //gameObject.GetComponent<LineRenderer>().SetPositions(array);
+                if (hit.collider == null)
+                {
+                    Debug.Log("Do nothing");
+                    //do nothing
+                }
+                else if (hit.collider.gameObject.tag == "Enemy")
+                {
+                    hit.collider.SendMessageUpwards("OnDamage", -1);
+                }
+                else if (hit.collider != null)
+                {
+                    Debug.Log(hit.collider.gameObject.tag);
+                }
+            }
+        }
+
+        void Melee(bool melee)
+        {
+            if (melee && currentFireCooldown <= 0.0f)
+            {
+                currentFireCooldown = fireCooldown;
+                RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x + (playerWidth * lastDir), transform.position.y), new Vector2(lastDir, 0), playerWidth*2);
+                //Vector3[] array = { new Vector3(transform.position.x + (playerWidth * lastDir), transform.position.y,0),new Vector3(transform.position.x + (playerWidth * lastDir) + 20, transform.position.y, 0) };
+                //gameObject.GetComponent<LineRenderer>().SetPositions(array);
+                if (hit.collider == null)
+                {
+                    Debug.Log("Do nothing");
+                    //do nothing
+                }
+                else if (hit.collider.gameObject.tag == "Enemy")
+                {
+                    hit.collider.SendMessageUpwards("OnDamage", -1);
+                }
+                else if (hit.collider != null)
+                {
+                    Debug.Log(hit.collider.gameObject.tag);
+                }
+            }
+        }
+
     }
 }
