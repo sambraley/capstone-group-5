e45ec293cc7645d1de004d607ac8458981fe3ffe
Christopher Komplin <Christopher.Komplin@utexas.edu>
2016-09-28 15:53:54 -0500

    tiled background ez pz no bugs

---
+++ TwoPiece/Assets/Scripts/tileBackground.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class tileBackground : MonoBehaviour {
+
+    [SerializeField] private GameObject tile;
+    [SerializeField] private int xOffset = 5;
+    [SerializeField] private int yOffset = 2;
+    [SerializeField] private int xLength = 15;
+    [SerializeField] private int yLength = 30;
+    
+    [SerializeField] private List<GameObject> listSpecialTiles = new List<GameObject>();
+    private List<GameObject> listTiles = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        //The Bullet instantiation happens here.
+        if (transform.position.x == 0 && transform.position.y == 0)
+        {
+            GameObject tileCreated = null;
+            for (int x = 0; x < xLength; x++)
+            {
+                for (int y = 0; y < yLength; y++)
+                {
+                    if (x == 0 && y == 0)
+                        continue;
+                    if (x % xOffset == 0 && y % yOffset == 0)
+                    {
+                        tileCreated = Instantiate(listSpecialTiles[((y*yOffset)%(3*yOffset))/yOffset], new Vector3(transform.position.x + 2 * x, transform.position.y + 2 * y, 1), new Quaternion()) as GameObject;
+                    }
+                    else
+                    {
+                        tileCreated = Instantiate(tile, new Vector3(transform.position.x + 2*x, transform.position.y + 2*y, 1), new Quaternion()) as GameObject;
+                    }
+                    listTiles.Add(tileCreated);
+                }
+            }
+        }
+
+    }
+}
