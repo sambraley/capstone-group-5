2a5106bbf4f3fb0b13719b49374338ed30a07606
Christopher Komplin <Christopher.Komplin@utexas.edu>
2016-10-05 16:50:35 -0500

    jump bug fix

--- TwoPiece/Assets/Scripts/Player2D.cs
+++ TwoPiece/Assets/Scripts/Player2D.cs
@@ -197,6 +197,7 @@ namespace UnityStandardAssets._2D
                 // Add a vertical force to the player.
                 m_Grounded = false;
                 //m_Anim.SetBool("Ground", false);
+                m_Rigidbody2D.velocity = (new Vector2(m_Rigidbody2D.velocity.x, 0));
                 m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
                 //m_Anim.SetBool("isJumping", true);
             }
