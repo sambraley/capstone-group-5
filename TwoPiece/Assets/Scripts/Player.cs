81a29954418033781fda0a91e3327f964c5ec2e0
Samuel Braley <samuel.braley@utexas.edu>
2016-10-12 16:41:16 -0500

    added trigger back in

--- TwoPiece/Assets/Scripts/Player.cs
+++ TwoPiece/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 [RequireComponent(typeof(Controller2D))]
@@ -6,6 +7,18 @@ using System.Collections;
 [RequireComponent(typeof(SpriteRenderer))]
 public class Player : MonoBehaviour
 {
+    const float talkCooldown = 1.0f;
+    public float currenttalkCooldown = 0.0f;
+    bool onLadder = false;
+    bool onDialogue = false;
+    public bool hasSword = false;
+    Collider2D dialogueCollider;
+    Text dPrompt;
+    int coins = 0;
+    public int health = 3;
+    public int maxHealth = 3;
+    public int keys = 0;
+    public float damageTakenCooldown;
 
     public float jumpHeight = 4;
     public float timeToJumpApex = .4f;
@@ -39,6 +52,11 @@ public class Player : MonoBehaviour
     void Update()
     {
         UpdateCooldowns();
+
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Return))
+        {
+            Talk();
+        }
 
         if (controller.collisions.above || controller.collisions.below)
         {
@@ -87,4 +105,100 @@ public class Player : MonoBehaviour
         dashCooldown -= Time.deltaTime;
     }
 
+    public void Talk()
+    {
+        if (currenttalkCooldown <= 0.0f)
+        {
+            currenttalkCooldown = talkCooldown;
+            dialogueCollider.SendMessageUpwards("NextDialogue");
+        }
+    }
+
+    //--------------------------------------------------Trigger logic from here down----------------------------------------------------
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.tag);
+        if (other.gameObject.tag == "Ladder")
+        {
+            onLadder = true;
+            Rigidbody2D rigid = GetComponent<Rigidbody2D>();
+            rigid.gravityScale = 0;
+            //m_Anim.SetBool("isClimbing", true);
+        }
+        else if (other.gameObject.tag == "Dialogue")
+        {
+            onDialogue = true;
+            dialogueCollider = other;
+            dPrompt.enabled = true;
+            gameObject.SendMessage("PromptSet", true);
+        }
+        else if (other.gameObject.tag == "MeleeCone")
+        {
+            Debug.Log("Yarr you've been damaged");
+        }
+        else if (other.gameObject.tag == "Coin")
+        {
+            Destroy(other.gameObject);
+            ++coins;
+            gameObject.SendMessage("SetCoin", coins);
+        }
+        else if (other.gameObject.tag == "Bandana")
+        {
+            Destroy(other.gameObject);
+            if (health < maxHealth)
+            {
+                ++health;
+                gameObject.SendMessage("AddBandana");
+            }
+        }
+        else if (other.gameObject.tag == "Key")
+        {
+            Destroy(other.gameObject);
+            ++keys;
+            if (keys == 1)
+                gameObject.SendMessage("ToggleKey");
+        }
+        else if (other.gameObject.tag == "Door")
+        {
+            if (keys > 0)
+            {
+                --keys;
+                other.gameObject.SendMessage("OpenDoor");
+                if (keys == 0)
+                    gameObject.SendMessage("ToggleKey");
+            }
+        }
+        else if (other.gameObject.tag == "EnemyWeapon" && damageTakenCooldown <= 0.0f)
+        {
+            health--;
+            SendMessage("RemoveBandana");
+            damageTakenCooldown = 0.5f;
+        }
+        else if (other.gameObject.tag == "Sword")
+        {
+            hasSword = true;
+            Destroy(other.gameObject);
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ladder")
+        {
+            //Debug.Log("Not colliding with ladder");
+            onLadder = false;
+            //m_Anim.SetBool("isClimbing", false);
+            Rigidbody2D rigid = GetComponent<Rigidbody2D>();
+            rigid.gravityScale = 1;
+        }
+        else if (other.gameObject.tag == "Dialogue")
+        {
+            //Debug.Log("exiting dialogue range");
+            onDialogue = false;
+            dPrompt.enabled = false;
+            dialogueCollider.SendMessageUpwards("CloseDialogue");
+            dialogueCollider = null;
+            gameObject.SendMessage("PromptSet", false);
+        }
+    }
 }
