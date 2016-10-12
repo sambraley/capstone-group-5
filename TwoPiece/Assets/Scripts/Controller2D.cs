0f42bfa6482a807f7942fb09d7abb0ceaa19833b
Samuel Braley <samuel.braley@utexas.edu>
2016-10-11 20:46:22 -0500

    Fixed crate prefab

--- TwoPiece/Assets/Scripts/Controller2D.cs
+++ TwoPiece/Assets/Scripts/Controller2D.cs
@@ -7,7 +7,7 @@ public class Controller2D : MonoBehaviour
 
     public LayerMask collisionMask;
 
-    const float skinWidth = .015f;
+    public float skinWidth = .015f;
     public int horizontalRayCount = 4;
     public int verticalRayCount = 4;
 
---
+++ TwoPiece/Assets/Scripts/Crate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Controller2D))]
+public class Crate : MonoBehaviour {
+    public float gravity = -20;
+    Vector3 velocity;
+
+    Controller2D controller;
+    // Use this for initialization
+    void Awake () {
+        controller = GetComponent<Controller2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
