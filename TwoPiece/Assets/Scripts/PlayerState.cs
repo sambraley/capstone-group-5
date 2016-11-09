d0a676e53dafba10dda278247622aed339b3b8a3
Doug Wiley <wowdoug@gmail.com>
2016-11-09 16:19:16 -0600

    Player state singleton

---
+++ TwoPiece/Assets/Scripts/PlayerState.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class PlayerState
+{
+    private static PlayerState instance;
+    private int coins;
+
+    private PlayerState() {
+        coins = 0;
+    }
+    
+    public void setCoins(int coins)
+    {
+        this.coins = coins;
+    }
+
+    public int getCoins()
+    {
+        return coins;
+    }
+
+    public static PlayerState Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new PlayerState();
+            }
+            return instance;
+        }
+    }
+}
